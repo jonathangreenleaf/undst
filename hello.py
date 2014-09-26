8fb303c03d81a5ecf9a9b7a6c789dc837a8f0940
jonathangreenleaf <jonathangreenleaf@gmail.com>
2014-09-26 11:16:34 -0700

    move the Flask API inside main Nginx -> Django/Gunicorn setup

--- hello.py
+++ hello.py
@@ -5,23 +5,30 @@ from flask import Flask, make_response
 from werkzeug.contrib.fixers import ProxyFix
 app = Flask(__name__)
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
+@app.route('/flask', defaults={'path': ''})
+@app.route('/flask/<path:path>')
 def catch_all(path):
-    msg = '''please enter the api request like:<br>
+    msg = '''
+            API example using Flask, numpy, pandas <br>
+            pointed at an -> <a href ="http://www.hdfgroup.org/HDF5/">HDF5</a> binary data store <br>
+            Nginx is pointed at a vanilla Flask server for this, Gunicorn for the rest of the site<br><br>
+
+            this is driven by this <a href="https://github.com/jonathangreenleaf/undst">code</a> <br><br>
+
+            please enter the api request like:<br>
             {flask server}/api/v1.0/temp/begin_utc/end_utc/format<br>
             begin_utc / end_utc are unix timestamps (int)<br>
             format is either csv or json (string)<br>
-            e.g. <a href="http://jonathangreenleaf.com/api/v1.0/temp/1405083600/1405083640/json">
-            http://jonathangreenleaf.com/api/v1.0/temp/1405083600/1405083640/json</a><br>
-            or <a href="http://jonathangreenleaf.com/api/v1.0/temp/1405083600/1405083640/json">
-            http://jonathangreenleaf.com/api/v1.0/temp/1405083600/1405083640/csv</a>'''
+            e.g. <a href="http://jonathangreenleaf.com/flask/api/v1.0/temp/1405083600/1405083640/json">
+            http://jonathangreenleaf.com/flask/api/v1.0/temp/1405083600/1405083640/json</a><br>
+            or <a href="http://jonathangreenleaf.com/flask/api/v1.0/temp/1405083600/1405083640/json">
+            http://jonathangreenleaf.com/flask/api/v1.0/temp/1405083600/1405083640/csv</a>'''
     return msg + '<br>You entered: %s' % path
 
 if __name__ == '__main__':
     app.run()
 
-@app.route('/api/v1.0/temp/<int:begin_utc>/<int:end_utc>/<format>', methods = ['GET'])
+@app.route('/flask/api/v1.0/temp/<int:begin_utc>/<int:end_utc>/<format>', methods = ['GET'])
 def get_temps(begin_utc, end_utc, format):
 
     store = pd.HDFStore('data.h5')
